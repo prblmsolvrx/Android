0b3270e31ac3331b669ee3eb1f8772d99622a201
Soumik Ghosh <realgoblu@gmail.com>
2024-01-01 19:22:08 +0530

    1st jan

---
+++ AndroidStudioProjects/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.yourappname
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val helloTextView: TextView = findViewById(R.id.helloTextView)
+        helloTextView.setOnClickListener {
+            // You can add any action you want when the TextView is clicked
+            // For now, let's just change the text
+            helloTextView.text = "Hello Clicked!"
+        }
+    }
+}
